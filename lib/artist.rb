c03c3fc47e3a269092b873aa0170af009a7ff38f
Amil Masic <amil.masic@gmail.com>
2018-09-26 19:57:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require "pry"
 class Artist
   attr_accessor :name, :songs
-
+  @@song_count = 0
   def initialize(name)
     @name = name
     @songs = []
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.song_count
-    
+
   end
 
 
